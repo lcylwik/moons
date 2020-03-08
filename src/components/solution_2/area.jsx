ce9dc33259b1d15bcd2bad37ba382f6a02710d01
Lianet Cylwik <cylwiklianet@gmail.com>
2020-03-08 15:21:38 -0600

    component alternativo

---
+++ src/components/solution_2/area.jsx
@@ -0,0 +1,72 @@
+import React, { useEffect, useRef } from 'react';
+import * as d3 from 'd3';
+
+const Area = ({ id, data, width, height }) => {
+
+    const ref = useRef(null);
+    const margin = 5;
+    const h = height - 2 * margin, w = width - 2 * margin
+
+    const x = d3.scaleLinear()
+        .domain(d3.extent(data, d => d.a)) 
+        .range([margin, w])
+
+    const y = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.b)]) 
+        .range([h, margin]);
+
+    const area = d3.area()
+        .x(d => x(d.a))
+        .y0(height)
+        .y1(d => y(d.b))
+        .curve(d3.curveBasis);
+ 
+    useEffect(() => {
+        const group = d3.select(ref.current);
+        const groupWithData = group.selectAll("g").data(data);
+        groupWithData.exit().remove();
+
+        const groupWithUpdate = groupWithData.enter()
+            .append("g")
+
+        const path = groupWithUpdate.append("path")
+            .merge(groupWithData.select("path"));
+
+        const clipPath = groupWithUpdate.append("clipPath");
+
+        clipPath.attr("id", `MiClip_${id}`)
+            .append("path").attr("d", area(data))
+
+        const circle_1 = groupWithUpdate
+            .append("circle")
+            .attr("id", "circulo_1")
+            .attr("cx", "75")
+            .attr("cy", "0")
+            .attr("r", "85")
+
+        const circle_2 = groupWithUpdate
+            .append("circle")
+            .attr("id", "circulo_2")
+            .attr("cx", "75")
+            .attr("cy", "0")
+            .attr("r", "90")
+
+        const use = groupWithUpdate
+            .append("use")
+            .attr("clip-path", `url(#MiClip_${id})`)
+            .attr("xlink:href", "#circulo_1")
+            .attr("class", (d, i) => {
+                return `curve_${id}`;
+            })
+    }, [])
+
+
+    return (
+        <svg width={200} height={200}>
+            <g ref={ref} transform={`translate(${100 / 4} ${200 / 2})`} />
+        </svg>
+
+    )
+}
+
+export default Area;
---
+++ src/components/solution_2/element.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import Circle from './circle';
+import Description from './description';
+
+class ElementAlternativo extends Component {
+
+  render() {
+    const infoElement = this.props.info;
+    const wH = 200;
+    const [ wInt, hInt] = [170, 89];
+
+    const propsGeneric = {
+      ...infoElement,
+      colorTable: `color-${infoElement.id}-1`,
+      colorSmartphone: `color-${infoElement.id}-2`
+    }
+
+    const propsCircle = {
+      ...propsGeneric, wH, wInt, hInt
+    }
+
+    return (
+      <div className="grafica col-md-4">
+        <Circle dataCircle={propsCircle} ></Circle>
+        <Description dataCircle={propsGeneric}></Description>
+        <div className="col-md-12">
+          <hr />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default ElementAlternativo;
